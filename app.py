0bc5571dbbee92028ef70f82e251afd53a43c3c9
Vibhum Sharma <vibhum10sharma@gmail.com>
2025-05-01 13:14:27 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ uploaded_file = st.file_uploader("Upload a Chest X-ray Image", type=["jpg", "jpe
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    st.image(img, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess image
     img_resized = img.resize((224, 224))  # Adjust size as per your model
